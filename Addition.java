e383bfe3ee8088849f735ba25cb3fccec0d90339
tanvi-patel-112 <tanvipateltjp2002@gmail.com>
2025-07-09 16:10:02 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package ANP_D1485;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int sum =a + b ;
+		System.out.println("sum:"+sum);
+	}
+
+}
